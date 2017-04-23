4277c1a7fd77140858238c9e5c10bb9c2f8aaf3a
Raviteja43 <graviteja_43@rediffmail.com>
2017-04-23 16:54:53 +0530

    Added changes to Mem_Reports.java
    Added changes to Mem_Reports.java

--- app/src/main/java/com/tracker/student/ketto/Mem_Reports.java
+++ app/src/main/java/com/tracker/student/ketto/Mem_Reports.java
@@ -2,18 +2,67 @@ package com.tracker.student.ketto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.ScrollView;
+import android.widget.Spinner;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Mem_Reports extends AppCompatActivity {
-    @BindView(R.id.Radio_Milan);
-
-
+    @BindView(R.id.spinner_Milan)
+    Spinner spinnerMilan;
+    @BindView(R.id.spinner_Khanda)
+    Spinner spinnerKhanda;
+    @BindView(R.id.radio_Weekly)
+    RadioButton radioWeekly;
+    @BindView(R.id.radio_Monthly)
+    RadioButton getRadioMonthly;
+    @BindView(R.id.radio_MKGroup)
+    RadioGroup radioMKGroup;
+    @BindView(R.id.Attendence_Report)
+    ScrollView attendanceReport;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mem__reports);
+        ButterKnife.bind(this);
+        /* Invoking Populate Spinners Method*/
+        populateSpinners();
+        /* Invoking getReport Method */
+        Button viewReport = (Button) findViewById(R.id.get_Report);
+        viewReport.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getReport();
+            }
+        });
+    }
+    public void populateSpinners(){
+        /* populating Milan Spinner */
+
+        ArrayAdapter<CharSequence> adapterMilan = ArrayAdapter.createFromResource(this,
+                R.array.milan_list, android.R.layout.simple_spinner_item);
+        adapterMilan.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMilan.setAdapter(adapterMilan);
+        /* populating Khanda Spinner */
+
+        ArrayAdapter<CharSequence> adapterKhanda = ArrayAdapter.createFromResource(this,
+                R.array.khanda_list, android.R.layout.simple_spinner_item);
+        adapterKhanda.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerKhanda.setAdapter(adapterKhanda);
 
     }
+    public void getReport(){
+        String milanVal=spinnerMilan.getSelectedItem().toString().trim();
+        String khandaVal=spinnerKhanda.getSelectedItem().toString().trim();
+        int radioId=radioMKGroup.getCheckedRadioButtonId();
+        RadioButton selctedButton=(RadioButton)findViewById(radioId);
+        String radioText=selctedButton.getText().toString();
+
+    }
+
 }
